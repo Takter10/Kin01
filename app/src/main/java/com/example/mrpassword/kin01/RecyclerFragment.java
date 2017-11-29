b07bd93a4b8469dc1a263ad9374f53d2747ec5e4
Takter10 <netiwut.ka.58@ubu.ac.th>
2017-11-29 14:21:54 +0700

    fragment

--- app/src/main/java/com/example/mrpassword/kin01/RecyclerFragment.java
+++ app/src/main/java/com/example/mrpassword/kin01/RecyclerFragment.java
@@ -24,7 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class RecyclerFragment extends Fragment {
     private int numberOfItem;
     private RecyclerView rv;
-    private RVAdapter adapter;
+//    private RVAdapter adapter;
     private Toolbar toolbar2;
 
     DatabaseReference Food ;
@@ -45,23 +45,23 @@ public class RecyclerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_recycler,container,false);
-        toolbar2 = v.findViewById(R.id.toolbar2);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar2);
-        ActionBar ab = ((AppCompatActivity)getActivity()).getSupportActionBar();
-        ab.setHomeButtonEnabled(true);
-        ab.setDisplayHomeAsUpEnabled(true);
+        View v = inflater.inflate(R.layout.fragment_content,container,false);
+//        toolbar2 = v.findViewById(R.id.toolbar2);
+//        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar2);
+//        ActionBar ab = ((AppCompatActivity)getActivity()).getSupportActionBar();
+//        ab.setHomeButtonEnabled(true);
+//        ab.setDisplayHomeAsUpEnabled(true);
         //Firebase
         FirebaseDatabase database ;
         DatabaseReference databaseReference;
         //Firebase End
-
-        rv = v.findViewById(R.id.recycler);
-        if(savedInstanceState != null)
-            numberOfItem = getArguments().getInt("nof_item");
-        adapter = new RVAdapter(numberOfItem);
-        rv.setLayoutManager(new LinearLayoutManager(container.getContext()));
-        rv.setAdapter(adapter);
+//
+//        rv = v.findViewById(R.id.recycler);
+//        if(savedInstanceState != null)
+//            numberOfItem = getArguments().getInt("nof_item");
+//        adapter = new RVAdapter(numberOfItem);
+//        rv.setLayoutManager(new LinearLayoutManager(container.getContext()));
+//        rv.setAdapter(adapter);
         return v;
     }
 
@@ -70,33 +70,33 @@ public class RecyclerFragment extends Fragment {
         ((MainActivity)getActivity()).toggle.syncState();
     }
 
-    private class RVAdapter extends RecyclerView.Adapter<VHolder>{
-        int nof_item;
-        RVAdapter(int nof_item){
-            this.nof_item = nof_item;
-        }
-        @Override
-        public VHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View card = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.card,parent,false
-                    );
-            return new VHolder(card);
-        }
-
-        @Override
-        public void onBindViewHolder(VHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return nof_item;
-        }
-    }
-
-    private class VHolder extends RecyclerView.ViewHolder{
-        public VHolder(View itemView) {
-            super(itemView);
-        }
-    }
+//    private class RVAdapter extends RecyclerView.Adapter<VHolder>{
+//        int nof_item;
+//        RVAdapter(int nof_item){
+//            this.nof_item = nof_item;
+//        }
+//        @Override
+//        public VHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            View card = LayoutInflater.from(parent.getContext())
+//                    .inflate(R.layout.card,parent,false
+//                    );
+//            return new VHolder(card);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(VHolder holder, int position) {
+//
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return nof_item;
+//        }
+//    }
+//
+//    private class VHolder extends RecyclerView.ViewHolder{
+//        public VHolder(View itemView) {
+//            super(itemView);
+//        }
+//    }
 }
